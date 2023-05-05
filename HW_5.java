fee9232e52b9d765c0239e84bf71fe18438c2ab5
Dasha <daryaishchuk@gmail.com>
2023-05-05 14:41:27 +0300

    HW-5

---
+++ HW_5.java
@@ -0,0 +1,128 @@
+// task 1
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class HW_5 {
+    
+       // Метод, который добавляет номера в книгу
+    public static void addNumber(String key, int value, Map<String, ArrayList<Integer>> map){
+        if (map.containsKey(key)) {
+            map.get(key).add(value);
+        } else {
+            ArrayList<Integer> list = new ArrayList<>();
+            list.add(value);
+            map.put(key, list);
+        }
+
+    }
+    // Метод, который печатает список контактов
+    public static void printBook(Map<String, ArrayList<Integer>> map){
+        for (var item : map.entrySet()) {
+            String phones = "";
+            for(int el: item.getValue()){
+                phones = phones + el + ", ";
+            }
+            System.out.printf("%s: %s \n", item.getKey(), phones);
+        }
+    }
+    public static void main(String[] args) {
+        Map<String, ArrayList<Integer>> bookPhone = new HashMap<>();
+        addNumber("Ivanov", 123, bookPhone);
+        addNumber("Ivanov", 1234, bookPhone);
+        addNumber("Petrov", 546585, bookPhone);
+        addNumber("Sidorov", 8956477, bookPhone);
+        addNumber("Ivanov", 12356233, bookPhone);
+        addNumber("Petrov", 787897, bookPhone);
+        printBook(bookPhone);
+       }
+}
+
+// task 2
+
+package HomeWork_5;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+
+// Иван Иванов
+// Светлана Петрова
+// Кристина Белова
+// Анна Мусина
+// Анна Крутова
+// Иван Юрин
+// Петр Лыков
+// Павел Чернов
+// Петр Чернышов
+// Мария Федорова
+// Марина Светлова
+// Мария Савина
+// Мария Рыкова
+// Марина Лугова
+// Анна Владимирова
+// Иван Мечников
+// Петр Петин
+// Иван Ежов
+// Написать программу, которая найдет и выведет повторяющиеся имена с количеством повторений.
+// Отсортировать по убыванию популярности Имени.
+
+public class HW_5 {
+    public static void main(String[] args) {
+        Map<String, Integer> namePeople = new HashMap<>();
+        String[] staff = {
+                "Иван Иванов",
+                "Светлана Петрова",
+                "Кристина Белова",
+                "Анна Мусина",
+                "Анна Крутова",
+                "Иван Юрин",
+                "Петр Лыков",
+                "Павел Чернов",
+                "Петр Чернышов",
+                "Мария Федорова",
+                "Марина Светлова",
+                "Мария Савина",
+                "Мария Рыкова",
+                "Марина Лугова",
+                "Анна Владимирова",
+                "Иван Мечников",
+                "Петр Петин",
+                "Иван Ежов"
+        };
+        countName(staff, namePeople);
+        sortedStaff(namePeople);
+
+    }
+
+    // Функция, считающая кол-во повторений имен сотрудников
+    public static void countName(String[] people, Map<String, Integer> namePeople) {
+        for (String person : people) {
+            String name = person.split(" ")[0];
+            if (namePeople.containsKey(name)) {
+                namePeople.put(name, namePeople.get(name) + 1);
+            } else {
+                namePeople.put(name, 1);
+            }
+        }
+    }
+
+    // Функция сотртировки по убыванию популярности имени
+    public static void sortedStaff(Map<String, Integer> namePeople) {
+        Map<String, Integer> sortedName = namePeople.entrySet().stream()
+                .sorted(Comparator.comparingInt(e -> -e.getValue()))
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        (a, b) -> {
+                            throw new AssertionError();
+                        },
+                        LinkedHashMap::new));
+
+        sortedName.entrySet().forEach(System.out::println); 
+    }
+}
